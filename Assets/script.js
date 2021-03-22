c33c54fd9a857f6ec78021364ecb99edc8818283
Eric Dodgion <Barry.manowar@gmail.com>
2021-03-21 21:25:28 -0600

    second commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,57 +1,57 @@
+//current time through moment.js
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY, hh:mm:ss"));
 
 var time = moment();
 var hour = moment().hours()
-var past = moment().startOf('day').add(9, "hours");
 
 function startSchedule() {
-   
-$(".time-block").each(function() {
-    var id = $(this).attr("id")
-    var planner = localStorage.getItem(id)
+  $(".time-block").each(function () {
+    //current HMTL element selecting attribute "id" and storing it locally
+    var id = $(this).attr("id");
+    var description = localStorage.getItem(id);
 
-    if(planner !== null) {
-        $(this).children(".col-md-9").val(planner)
+    if (description !== null) {
+      $(this).children(".col-md-9").val(description);
     }
-    });
+  });
 }
 
 startSchedule();
-var saveBtn = $(".saveBtn")
-saveBtn.on("click", function() {
-    var value = $(this).siblings(".description").val();
-   var time = $(this).parent().attr("id");
-      
-       localStorage.setItem(time, value);
-   });
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().find(".hour").attr("data-hour");
 
+//using string literal to pull the hour ID and variable together for local storage
+  localStorage.setItem(`hour-${time}`, value);
+});
 
-function timeColor() { 
-var hour = time.hours()
-$(".description").each(function() {
-    var present = parseInt($(this).attr("id"));
-    if (present === hour) {
-        $(this).addClass("present").css("background-color:", "red");
-        console.log(time)
+function timeColor() {
+  $(".description").each(function () {
+    //moment format to 24hr clock
+      var currentTime  = parseInt(moment().format("H"));
+    var hourBlock = parseInt($(this).parent().find(".hour").attr("data-hour"));
+   
+    if (hourBlock === currentTime) {
+      $(this).addClass("present")
+
+    } else if (hourBlock > currentTime) {
+      $(this).addClass("future")
+    
+    } else {
+      $(this).addClass("past")
     }
-    else if (present > hour) {
-        $(this).addClass("future").css("background-color:", "green");
-        console.log(time)
-    } 
-    else { (present === past)
-        $(".description").addClass("past").css("background-color:", "gray");
-}
-}); 
-
+  });
 }
 
 timeColor();
-
-
-var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-for (var i = 0; i < x.length; i++) {
-    var dataHour = localStorage.getItem(x[i]);
-    $(".descrtiption" + x[i]).val(dataHour);
+//loop using pulling each id and storing the values indiviually
+for (var i = 9; i < 24; i++) {
+  var localStorageData = localStorage.getItem(`hour-${i}`);
+  if (localStorageData != null) {
+    $(`#hour-${i}`).val(localStorageData);
+  }
 }
 
--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 # 05 Third-Party APIs: Work Day Scheduler
-
-## Your Task
-
+## Criteria
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
 
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
@@ -30,5 +16,11 @@ THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
 
+
+## Live Link
+
 ## What it should look like
 ![](./Assets/05-third-party-apis-homework-demo.gif)
+
+
+## Credit
